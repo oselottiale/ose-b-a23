c691128b39cca0b281063ed20ec63b678cc689d6
oseale <aleksi.piitulainen4@outlook.com>
2025-11-10 08:52:00 +0200

    Refactor Firebase initialization and auth setup
    Removed imports and updated Firebase initialization.

--- firebase.js
+++ firebase.js
@@ -1,13 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
   apiKey: "AIzaSyBey9z51rXWU99ZtDaBP5DEEIQ1Nt-VxhA",
   authDomain: "repe-229f0.firebaseapp.com",
   projectId: "repe-229f0",
   appId: "1:492186174522:web:bdae6cd85f8c7a6d74a103"
 };
+firebase.initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
+window.auth = firebase.auth();
 
-export const auth = getAuth(app);
